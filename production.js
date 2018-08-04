8f81f73c701c36697c30796a86896e1f87edfcde
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-08-05 02:43:27 +0700

    Edited greeting prefix by move prefix to global constant variable

--- production.js
+++ production.js
@@ -1,9 +1,11 @@
+const PREFIX_MESSAGE = "Hello"
+
 const SayHello = (message) => {
     return greeting(message)
 }
 
 const greeting = (message) => {
-    return `Hello ${message}`
+    return `${PREFIX_MESSAGE} ${message}`
 }
 
 module.exports = {
